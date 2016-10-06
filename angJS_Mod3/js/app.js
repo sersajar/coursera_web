56ddb0258623865bdcbaa11d30a9d378601828ec
Sergi Sabate <sersajar@gmail.com>
2016-10-06 12:17:57 +0200

    starting week3

---
+++ angJS_Mod3/js/app.js
@@ -0,0 +1,16 @@
+;(function(){
+	'use strict'
+
+	/**
+	* NarrowItDownApp Module
+	*
+	* Description
+	*/
+	angular.module('NarrowItDownApp', [])
+	.controller('NarrowItDownAppController', NarrowItDownAppController)
+	.service('MenuSearchService', MenuSearchService);
+
+	
+
+
+})();
