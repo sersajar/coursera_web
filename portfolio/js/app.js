52ae6a6b9a9850e7653399bd46b893c01de8ae58
Sergi Sabate <sersajar@gmail.com>
2016-10-23 04:04:04 +0200

    new images

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
+	$("#logo-jumbo").click(function() {
+		var randomColor = getRandomColor();
+		$(".jumbotron").css('background-color', randomColor);
+		$("footer").css('background-color', randomColor);
+	});
 
-        $("#cbd").click(function(){
-            $('#cbdModal').modal('toggle');
-        });
+});
 
-        $( "#aToggle" ).click(function() {
-          	$('#aToggle').toggleClass('disabled');
 
-        });
+//function to get a random html color
 
-    });
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color);
+    return color;
+};
