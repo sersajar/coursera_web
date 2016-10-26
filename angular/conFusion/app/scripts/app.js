1e52c34cd38c2465700e55a870f196bcf3f92b2c
Sergi Sabate <sersajar@gmail.com>
2016-10-26 16:57:46 +0200

    week 3 -

--- angular/conFusion/app/scripts/app.js
+++ angular/conFusion/app/scripts/app.js
@@ -1,7 +1,13 @@
 'use strict';
-angular.module('confusionApp', []).controller('menuController', function() {
-  this.tab = 1;
-  this.filtText = '';
+angular.module('confusionApp', [])
+.controller('MenuController', ['$scope', function($scope) {
+  $scope.tab = 1;
+  $scope.filtText = '';
+  $scope.showDetails = false;
+
+  $scope.toggleDetails = function() {
+      $scope.showDetails = !$scope.showDetails;
+  };
 
   var dishes = [
     {
@@ -45,26 +51,27 @@ angular.module('confusionApp', []).controller('menuController', function() {
     }
   ];
 
-  this.dishes = dishes;
+  $scope.dishes = dishes;
 
-  this.select = function(setTab) {
-    this.tab = setTab;
+  $scope.select = function(setTab) {
+    $scope.tab = setTab;
 
     if (setTab === 2) {
-      this.filtText = "appetizer";
+      $scope.filtText = "appetizer";
     }
     else if (setTab === 3) {
-      this.filtText = "mains";
+      $scope.filtText = "mains";
     }
     else if (setTab === 4) {
-      this.filtText = "dessert";
+      $scope.filtText = "dessert";
     }
     else {
-      this.filtText = "";
+      $scope.filtText = "";
     }
   };
 
-  this.isSelected = function (checkTab) {
-    return (this.tab === checkTab);
+  $scope.isSelected = function (checkTab) {
+    return ($scope.tab === checkTab);
   };
-});
+
+}]);
