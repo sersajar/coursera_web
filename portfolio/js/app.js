e711eebe16b292f6cedf42d1b425ae38683fa52b
Sergi Sabate <sersajar@gmail.com>
2016-11-11 17:24:40 +0100

    working on 4x4

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){
-	$("#logo-jumbo").click(function() {
+'use strict';
+
+$(document).ready(function () {
+     
+	$("#logo-jumbo").click(function () {
 		var randomColor = getRandomColor();
 		$(".jumbotron").css('background-color', randomColor);
 		$("footer").css('background-color', randomColor);
 	});
 
-
+    $(".knowledgePics").click(function showPic() {
+        
+        if (this.id == "acc1") {
+            $("#chPic").prepend('<img src="img/germ.jpg">');   
+            this.id = undefined;  
+         } else if (this.id == "acc2") {
+            $("#chPic").prepend('<img src="img/botella.jpg">');
+             
+         } 
+    });
     // $('#aToggle').click(function(e) {
 
     //     $(this).toggleClass('disabled');
@@ -14,9 +26,11 @@ $(document).ready(function(){
     //});
 });
 
+//function to show accordion pictures
+
+
 
 //function to get a random html color
-
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
