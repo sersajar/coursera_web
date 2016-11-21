ad86f866b5dedb5cbc4e4d98598b291ddb331803
Sergi Sabate <sersajar@gmail.com>
2016-11-21 16:03:35 +0100

    update 4x4

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -1,23 +1,49 @@
 'use strict';
 
 $(document).ready(function () {
-     
+
 	$("#logo-jumbo").click(function () {
 		var randomColor = getRandomColor();
 		$(".jumbotron").css('background-color', randomColor);
 		$("footer").css('background-color', randomColor);
 	});
 
-    // $('#aToggle').click(function(e) {
+    $(".strainLink").click(function () {
 
-    //     $(this).toggleClass('disabled');
-    //     e.preventDefault();
-    //     console.log("hey");
-    //});
+        if ($(".onoff").hasClass('active')) {
+            $(".onoff").removeClass('active');
+        };
+
+        // if ($(".strainLink").attr('href', '#yumbolttab')) {
+        //     if ($(".onoff").hasClass('active')) {
+        //         $(".onoff").removeClass('active');
+        //     };
+        //     $("#go-to-yum").addClass('active');
+
+        // } else if ($(".strainLink").attr('href', '#onepeacetab')) {
+        //     if ($(".onoff").hasClass('active')) {
+        //         $(".onoff").removeClass('active');
+        //     };
+        //     $("#go-to-peace").addClass('active');
+
+        // }
+
+        // } else if ($(".strainLink").attr('href', '#masquetab')) {
+        //     $("#go-to-mask").addClass('active');
+
+        // } else if ($(".strainLink").attr('href', '#willowtab')) {
+        //     $("#go-to-wil").addClass('active');
+
+        // } else if ($(".strainLink").attr('href', '#criticaltab')) {
+        //     $("#go-to-kri").addClass('active');
+
+        // } else if ($(".strainLink").attr('href', '#tuttitab')) {
+        //     $("#go-to-tu").addClass('active');
+        // }
+    });
+
+
 });
-
-//function to show accordion pictures
-
 
 
 //function to get a random html color
